5ffe05d86021ebffa1a25863f13962fe2c026762
Omkar Dharkar <87490714+grasperIQ100@users.noreply.github.com>
2024-03-28 19:18:52 +0000

    Pending changes exported from your codespace

---
+++ src/components/introduction.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li style={{backgroundImage: 'url(https://media.giphy.com/media/3o7ZerdezTT72sWC1W/giphy.gif)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1 style={{ color: 'white' }}>Hi! <br />I'M OMKAR DHARKAR</h1>
+                          <p><a className="btn btn-primary btn-learn" style={{ color: 'white' }} href="https://www.linkedin.com/in/omkar-dharkar-23002b17a/" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{backgroundImage: 'url(https://media.giphy.com/media/3o7ZerdezTT72sWC1W/giphy.gif)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1 style={{ color: 'white' }}>I AM PASSIONATE ABOUT BUILDING<br /> THINGS !!</h1>
+                          <p><a className="btn btn-primary btn-learn" style={{ color: 'white' }} href="https://github.com/grasperIQ100" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
+          <aside id="colorlib-aside" className="border js-fullheight">
+            <div className="text-center">
+              <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
+              <h1 id="colorlib-logo"><a href="index.html">Omkar Dharkar</a></h1>
+              <span className="email"><i className="icon-mail"></i> dharkaromkar777@gmail.com</span>
+            </div>
+            <nav id="colorlib-main-menu" role="navigation" className="navbar">
+              <div id="navbar" className="collapse">
+                <ul>
+                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
+                  <li><a href="#about" data-nav-section="about">About Me</a></li>
+                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+                </ul>
+              </div>
+            </nav>
+            <nav id="colorlib-main-menu">
+              <ul>
+                <li><a href="https://www.facebook.com/omkar.dharkar.96/" target="_blank" rel="noopener noreferrer">Facebook <i className="icon-facebook2" /></a></li>
+                <li><a href="https://www.instagram.com/omkar_dharkar__/" target="_blank" rel="noopener noreferrer">Instagram <i className="icon-instagram" /></a></li>
+                <li><a href="https://www.linkedin.com/in/omkar-dharkar-23002b17a/" target="_blank" rel="noopener noreferrer">linkedin <i className="icon-linkedin2" /></a></li>
+                <li><a href="https://github.com/grasperIQ100" target="_blank" rel="noopener noreferrer">github <i className="icon-github"></i></a></li>
+              </ul>
+            </nav>
+            <div className="colorlib-footer">
+              <p><small>
+                  Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-beer" aria-hidden="true"></i><br></br>
+                  Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration
+              </small></p>
+              <p><small>
+                Something coming soon !!
+              </small></p>
+            </div>
+          </aside>
+        </div>
+      </div>
+    )
+  }
+}
